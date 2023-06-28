4a899b5167ba4f1ccd23a5d2f928058213888aac
ZhuRuoLing <jkl-963@live.com>
2023-06-28 23:02:54 +0800

    0.12.0 Controller fields reduce

--- src/main/java/net/zhuruoling/omms/client/controller/Controller.java
+++ src/main/java/net/zhuruoling/omms/client/controller/Controller.java
@@ -2,87 +2,21 @@ package net.zhuruoling.omms.client.controller;
 
 
 public class Controller {
+    private Controller(){}
     private String name;
-
-    private String executable;
-
     private String type;
-
-    private String launchParams;
-
-    private String workingDir;
-
     private boolean statusQueryable;
-
-    public boolean isStatusQueryable() {
-        return statusQueryable;
-    }
-
-    public void setStatusQueryable(boolean statusQueryable) {
-        this.statusQueryable = statusQueryable;
-    }
-
-    public Controller() {
-    }
-
-    public Controller(String name, String executable, String type, String launchCommand, String workingDir) {
-        this.name = name;
-        this.executable = executable;
-        this.type = type;
-        this.launchParams = launchCommand;
-        this.workingDir = workingDir;
-    }
 
     public String getName() {
         return name;
-    }
-
-    @Override
-    public String toString() {
-        return "Controller{" +
-                "name='" + name + '\'' +
-                ", executable='" + executable + '\'' +
-                ", type='" + type + '\'' +
-                ", launchParams='" + launchParams + '\'' +
-                ", workingDir='" + workingDir + '\'' +
-                ", statusQueryable=" + statusQueryable +
-                '}';
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getExecutable() {
-        return executable;
-    }
-
-    public void setExecutable(String executable) {
-        this.executable = executable;
     }
 
     public String getType() {
         return type;
     }
 
-    public void setType(String type) {
-        this.type = type;
-    }
-
-    public String getLaunchParams() {
-        return launchParams;
-    }
-
-    public void setLaunchParams(String launchParams) {
-        this.launchParams = launchParams;
-    }
-
-    public String getWorkingDir() {
-        return workingDir;
-    }
-
-    public void setWorkingDir(String workingDir) {
-        this.workingDir = workingDir;
+    public boolean isStatusQueryable() {
+        return statusQueryable;
     }
 
 }
