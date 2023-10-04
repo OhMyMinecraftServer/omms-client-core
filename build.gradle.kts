caff8bf1b6d76587bb79271783c4010c971b7fc8
ZhuRuoLing <jkl-963@live.com>
2023-10-04 22:54:08 +0800

    feat: add Controller#displayName

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,7 @@ repositories {
 }
 
 dependencies {
-    api("com.google.code.gson:gson:2.9.0")
+    implementation("com.google.code.gson:gson:2.9.0")
 }
 
 group = "net.zhuruoling"
--- src/main/java/net/zhuruoling/omms/client/controller/Controller.java
+++ src/main/java/net/zhuruoling/omms/client/controller/Controller.java
@@ -22,6 +22,6 @@ public class Controller {
     }
 
     public String getDisplayName() {
-        return displayName;
+        return displayName == null ? name : displayName;
     }
 }
