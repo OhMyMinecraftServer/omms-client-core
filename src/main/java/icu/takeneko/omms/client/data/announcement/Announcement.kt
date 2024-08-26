d7722a67839b6a85cac3d04bfb1ae52350813114
MercuryGryph <w84627302@gmail.com>
2024-08-26 17:30:42 +0800

    -

--- src/main/java/icu/takeneko/omms/client/data/announcement/Announcement.kt
+++ src/main/java/icu/takeneko/omms/client/data/announcement/Announcement.kt
@@ -8,6 +8,12 @@ data class Announcement(
     var title: String,
     var content: List<String>,
 ) {
+    constructor(
+        id: String = Util.randomStringGen(16),
+        timeMillis: Long = System.currentTimeMillis(),
+        title: String,
+        content: Array<String>,
+    ): this(id, timeMillis, title, content.toList())
     override fun toString() =
         "Announcement { id='$id', timeMillis=$timeMillis, title='$title', content=$content}"
 }
